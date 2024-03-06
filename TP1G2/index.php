482670d1dd361b9b082e7647c97a1a16110d4363
hdafa <simohamd.hdafa@gmail.com>
2024-03-06 21:15:48 +0100

    Création de l'arborescence des repertoires/fichiers du site web

---
+++ TP1G2/index.php
@@ -0,0 +1,7 @@
+/**
+    les étapes suivantes pour la prochaine séance :
+    création des pages (HTML/CSS) à la racine :
+        - 404 page introuvable
+        - profil "The Shawshank Redemption"
+        - listage de tous les films sans pagination 
+*/
