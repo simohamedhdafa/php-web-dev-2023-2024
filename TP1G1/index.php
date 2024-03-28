7a7e20a3f97694577b73932913810e514729def4
hdafa <simohamd.hdafa@gmail.com>
2024-03-28 10:14:52 +0000

    sys pagination complet

--- TP1G1/index.php
+++
@@ -1,7 +0,0 @@
-/**
-    les étapes suivantes pour la prochaine séance :
-    création des pages (HTML/CSS) à la racine :
-        - 404 page introuvable
-        - profil "The Shawshank Redemption"
-        - listage de tous les films sans pagination 
-*/
---
+++ TP1G1/index_.php
@@ -0,0 +1,7 @@
+/**
+    les étapes suivantes pour la prochaine séance :
+    création des pages (HTML/CSS) à la racine :
+        2 - 404 page introuvable
+        3 - profil "The Shawshank Redemption"
+        1 - listage de tous les films sans pagination 
+*/
